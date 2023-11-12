b82b25061523aff306f580bd01e402748c628029
Hoai Nam <hoainamkin03@gmail.com>
2023-11-12 20:26:48 +0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,24 +1,38 @@
 def parse(file_name):
-
     # Read the file and return the data
     with open(file_name, "r", encoding="utf-8") as file:
         lines = file.readlines()
         lines = [line.strip() for line in lines]
 
-    KB = []
+    # Initialize input Knowledge Base (input_kb) and Query lists
+    input_kb = []
     query = []
+
+    # Check if the first line is "TELL"
     if lines[0] == "TELL":
+        # Iterate through the lines starting from the second line
         for i in range(1, len(lines)):
+            # Check if the current line is "ASK"
             if lines[i] == "ASK":
+                # Append the next line to the query list and break the loop
                 query.append(lines[i+1])
                 break
             else:
-                KB = (lines[i].split(";"))
-                # remove the space in the KB
-                # Remove empty string
-                KB = [x.strip() for x in KB if x.strip()]
-    # split by " " for each element in the KB
-    for i in range(len(KB)):
-        KB[i] = KB[i].split(" ")
-        
-    return KB, query
+                # Split the current line by semicolon, remove spaces, and add to input_kb
+                input_kb = (lines[i].split(";"))
+                input_kb = [x.strip() for x in input_kb if x.strip()]
+
+    # Split each element in input_kb by space
+    for i in range(len(input_kb)):
+        input_kb[i] = input_kb[i].split(" ")
+
+    # Process input_kb: if there is '&' in the first element, split it and add the symbol
+    processed_kb = []
+    for sublist in input_kb:
+        if '&' in sublist[0]:
+            elements = sublist[0].split('&')
+            processed_kb.append([elements[0]] + ['&'] + [elements[1]] + ['=>'] + sublist[2:])
+        else:
+            processed_kb.append(sublist)
+
+    return processed_kb, query
