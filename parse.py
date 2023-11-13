d2199428b64e64dd4dd778d06ce760a8ad318861
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2023-11-13 11:14:48 +0700

    Remove redundant import

--- parse.py
+++ parse.py
@@ -25,17 +25,14 @@ def parse(file_name):
     return output_kb, output_query, symbols
 
 # Unit Tests
-import unittest
-
 class TestParse(unittest.TestCase):
-            
+
     def test_parse(self):
         # Test case for the parse function with a sample input file
         test_kb, test_query, test_symbols = parse("data.txt")
         self.assertEqual(test_kb, [['p2', '=>', 'p3'], ['p3', '=>', 'p1'], ['c', '=>', 'e'], ['b', '&', 'e', '=>', 'f'], ['f', '&', 'g', '=>', 'h'], ['p1', '=>', 'd'], ['p1', '&', 'p3', '=>', 'c'], ['a'], ['b'], ['p2']])
         self.assertEqual(test_query, ['d'])
         self.assertEqual(set(test_symbols), {'h', 'c', 'b', 'p3', 'f', 'p1', 'e', 'g', 'd', 'a', 'p2'})
-
 
 if __name__ == "__main__":
     unittest.main()
