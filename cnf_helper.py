6e8ad1ed19b1f0a3617779cbd3941a291f657ffb
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2023-11-19 02:33:03 +0700

    Add documentation and comments

--- cnf_helper.py
+++ cnf_helper.py
@@ -84,13 +84,25 @@ def distribute(sentence):
 ##############################################################################################################
 
 def get_previous_operand(sentence: list[str], index: int) -> tuple[int, int]:
-    # Process the parentheses
-    # Return the index of the outermost parentheses
+    """
+    Get the index of the operand preceding the operator at the given index.
 
+    Args:
+        sentence (list[str]): The sentence to check.
+        index (int): The index of the operator.
+
+    Raises:
+        ValueError: If the sentence is invalid.
+
+    Returns:
+        tuple[int, int]: The begin and end index of the operand preceding the operator.
+    """
+    # if the previous character is not a ')', then the operand is a single character
     if sentence[index - 1] != ')':
         return index - 1, index - 1
 
     parenthesis_stack = []
+    # iterate backwards from the operator
     for i in range(index - 1, -1, -1):
         if sentence[i] == ')':
             parenthesis_stack.append(i)
@@ -98,19 +110,34 @@ def get_previous_operand(sentence: list[str], index: int) -> tuple[int, int]:
             if len(parenthesis_stack) > 0:
                 parenthesis_stack.pop()
 
+        # if the parenthesis stack is empty, then the whole operand has been found
         if len(parenthesis_stack) == 0:
             begin = i
             return begin, index - 1
 
+    # if the parenthesis stack is not empty, then the sentence is invalid
     raise ValueError("Invalid sentence")
 
 def get_following_operand(sentence: list[str], index: int) -> tuple[int, int]:
-    # Process the parentheses
-    # Return the index of the outermost parentheses
-    parenthesis_stack = []
+    """
+    Get the index of the operand following the operator at the given index.
+
+    Args:
+        sentence (list[str]): The sentence to check.
+        index (int): The index of the operator.
+
+    Raises:
+        ValueError: If the sentence is invalid.
+
+    Returns:
+        tuple[int, int]: The begin and end index of the operand following the operator.
+    """
+    # if the next character is not a '(', then the operand is a single character
     if sentence[index + 1] != '(':
         return index + 1, index + 1
 
+    parenthesis_stack = []
+    # iterate forwards from the operator
     for i in range(index + 1, len(sentence)):
         if sentence[i] == '(':
             parenthesis_stack.append(i)
@@ -118,10 +145,12 @@ def get_following_operand(sentence: list[str], index: int) -> tuple[int, int]:
             if len(parenthesis_stack) > 0:
                 parenthesis_stack.pop()
 
+        # if the parenthesis stack is empty, then the whole operand has been found
         if len(parenthesis_stack) == 0:
             end = i
             return index + 1, end
 
+    # if the parenthesis stack is not empty, then the sentence is invalid
     raise ValueError("Invalid sentence")
 
 def add_parentheses_around_operator(sentence: list[str], operator: str) -> list[str]:
@@ -136,9 +165,13 @@ def add_parentheses_around_operator(sentence: list[str], operator: str) -> list[
         list[str]: The sentence with parentheses added.
     """
     idx = 0
+    # iterate through the sentence
     while idx < len(sentence):
+
+        # if the current character is the operator
         if sentence[idx] == operator:
 
+            # find the operands preceding the operator until the beginning of the sentence or a different operator
             before = idx
             while True:
                 begin, end = get_previous_operand(sentence, before)
@@ -146,6 +179,7 @@ def add_parentheses_around_operator(sentence: list[str], operator: str) -> list[
                 if before < 0 or sentence[before] != operator:
                     break
 
+            # find the operands following the operator until the end of the sentence or a different operator
             after = idx
             while True:
                 begin, end = get_following_operand(sentence, after)
@@ -153,13 +187,20 @@ def add_parentheses_around_operator(sentence: list[str], operator: str) -> list[
                 if after >= len(sentence) or sentence[after] != operator:
                     break
 
+            # if the operands are already enclosed in parentheses, then skip
             if sentence[before] == "(" and sentence[after] == ")":
                 idx = after
                 continue
 
+            # add parentheses around the operands
             sentence = sentence[:before+1] + ["("] + sentence[before+1:after] + [")"] + sentence[after:]
+
+            # skip to the end of the parentheses
             idx = after + 2
-        idx += 1
+
+        # if the current character is not the operator, then skip
+        else:
+            idx += 1
 
     return sentence
 
@@ -177,29 +218,43 @@ def simplify_parentheses(sentence: list[str]) -> list[str]:
     Returns:
         list[str]: The simplified sentence.
     """
+    # if the sentence does not contain any parentheses, then the sentence has been simplified
     if "(" not in sentence:
         return sentence
 
+    # get the main operator of the sentence
     main_operator = get_main_operator(sentence)
+
+    # if the sentence has no operator, then the sentence is enclosed in parentheses
     if main_operator == "":
         return simplify_parentheses(sentence[1:-1])
 
     idx = -1
+    # iterate through the sentence
     while idx < len(sentence):
+
+        # find the operands following the operator at the current index
         begin, end = get_following_operand(sentence, idx)
+
+        # if the operands are enclosed in parentheses
         if sentence[begin] == "(" and sentence[end] == ")":
 
+            # simplify the sub-sentence inside the parentheses and replace the sub-sentence with the simplified version
             sentence[begin+1:end] = (simplified := simplify_parentheses(sentence[begin+1:end]))
             end = begin + len(simplified) + 1
 
+            # if the sub-sentence has the same main operator as the sentence, or the sub-sentence is a single operand, then remove the parentheses
             sub_operator = get_main_operator(sentence[begin+1:end])
             if sub_operator == main_operator or sub_operator == "":
                 sentence = sentence[:begin] + sentence[begin+1:end] + sentence[end+1:]
                 idx = end - 1
+
+            # else, skip to the end of the parentheses
             else:
                 idx = end + 1
         else:
             idx = end + 1
+
     return sentence
 
 def get_main_operator(sentence: list[str]) -> str | None:
@@ -219,14 +274,26 @@ def get_main_operator(sentence: list[str]) -> str | None:
         str | None: The main operator that connects the operands in the sentence.
     """
     operator = ""
+
     idx = -1
+    # iterate through the sentence
     while idx < len(sentence):
+        # get to the next operator after the current index
         _, end = get_following_operand(sentence, idx)
         idx = end + 1
+
+        # if the index is out of bounds, then the sentence has no more operators
         if idx >= len(sentence):
             break
+
+        # if the operator is not set, then set the operator
         if operator == "":
             operator = sentence[idx]
+
+        # if the operator is already set, then check if the operator is the same as the previous operator
+        # if the operator is different, then the sentence has more than one operator. return None
         elif sentence[idx] != operator:
             return None
+
+    # return the operator
     return operator
