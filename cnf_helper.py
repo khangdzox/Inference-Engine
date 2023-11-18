e031a7c69250cc54e0ea010f9c219517145828d0
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2023-11-18 23:51:47 +0700

    Add CNF helper methods and unit tests.

--- cnf_helper.py
+++ cnf_helper.py
@@ -6,12 +6,12 @@ def and_or_tranformation(sentence : list[str]):
     # biconditional elemination
     while '<=>' in sentence:
         index = sentence.index('<=>')
-        sentence = sentence[:index-1] + [['(', '~', sentence[index-1], '||', sentence[index+1], ')'], '&', ['(', '~', sentence[index+1], '||', sentence[index-1], ')']] + sentence[index+2:]
+        sentence = sentence[:index-1] + ['(', '~', sentence[index-1], '||', sentence[index+1], ')', '&', '(', '~', sentence[index+1], '||', sentence[index-1], ')'] + sentence[index+2:]
 
     # implication elemination
     while '=>' in sentence:
         index = sentence.index('=>')
-        sentence = sentence[:index-1] + [['(', '~', sentence[index-1], '||', sentence[index+1], ')']] + sentence[index+2:]
+        sentence = sentence[:index-1] + ['(', '~', sentence[index-1], '||', sentence[index+1], ')'] + sentence[index+2:]
 
     # De Morgan's Law
     if '~' in sentence:
@@ -68,10 +68,10 @@ def distribute(sentence):
     print(operand_before)
     operand_after = sentence[and_index + 1::2]
     # remove all the '&' operator in operand_after
-    
+
     # Extract the AND operands
     and_operands = sentence[or_index + 2:and_index]
-    
+
     # Distribute '||' over '&'
     distributed_sentence = ['('] + operand_before + ['||', and_operands[0], ')']
 
@@ -79,5 +79,154 @@ def distribute(sentence):
         operand = operand_after.pop(0)
         distributed_sentence += ['&', '(', operand_before[0], '||',  operand, ')']
 
-
     return distributed_sentence
+
+##############################################################################################################
+
+def get_previous_operand(sentence: list[str], index: int) -> tuple[int, int]:
+    # Process the parentheses
+    # Return the index of the outermost parentheses
+
+    if sentence[index - 1] != ')':
+        return index - 1, index - 1
+
+    parenthesis_stack = []
+    for i in range(index - 1, -1, -1):
+        if sentence[i] == ')':
+            parenthesis_stack.append(i)
+        elif sentence[i] == '(':
+            if len(parenthesis_stack) > 0:
+                parenthesis_stack.pop()
+
+        if len(parenthesis_stack) == 0:
+            begin = i
+            return begin, index - 1
+
+    raise ValueError("Invalid sentence")
+
+def get_following_operand(sentence: list[str], index: int) -> tuple[int, int]:
+    # Process the parentheses
+    # Return the index of the outermost parentheses
+    parenthesis_stack = []
+    if sentence[index + 1] != '(':
+        return index + 1, index + 1
+
+    for i in range(index + 1, len(sentence)):
+        if sentence[i] == '(':
+            parenthesis_stack.append(i)
+        elif sentence[i] == ')':
+            if len(parenthesis_stack) > 0:
+                parenthesis_stack.pop()
+
+        if len(parenthesis_stack) == 0:
+            end = i
+            return index + 1, end
+
+    raise ValueError("Invalid sentence")
+
+def add_parentheses_around_operator(sentence: list[str], operator: str) -> list[str]:
+    """
+    Add parentheses to isolate the operands connected by the operator in the sentence.
+
+    Args:
+        sentence (list[str]): The sentence to add parentheses to.
+        operator (str): The operator to isolate.
+
+    Returns:
+        list[str]: The sentence with parentheses added.
+    """
+    idx = 0
+    while idx < len(sentence):
+        if sentence[idx] == operator:
+
+            before = idx
+            while True:
+                begin, end = get_previous_operand(sentence, before)
+                before = begin - 1
+                if before < 0 or sentence[before] != operator:
+                    break
+
+            after = idx
+            while True:
+                begin, end = get_following_operand(sentence, after)
+                after = end + 1
+                if after >= len(sentence) or sentence[after] != operator:
+                    break
+
+            if sentence[before] == "(" and sentence[after] == ")":
+                idx = after
+                continue
+
+            sentence = sentence[:before+1] + ["("] + sentence[before+1:after] + [")"] + sentence[after:]
+            idx = after + 2
+        idx += 1
+
+    return sentence
+
+def simplify_parentheses(sentence: list[str]) -> list[str]:
+    """
+    Simplify the parentheses in the sentence.
+
+    Remove the parantheses if the sub-sentence inside the parentheses has the same main operator as the sentence.
+
+    If the sentence does not contain any parentheses, then the sentence has been simplified.
+
+    Args:
+        sentence (list[str]): The sentence to simplify.
+
+    Returns:
+        list[str]: The simplified sentence.
+    """
+    if "(" not in sentence:
+        return sentence
+
+    main_operator = get_main_operator(sentence)
+    if main_operator == "":
+        return simplify_parentheses(sentence[1:-1])
+
+    idx = -1
+    while idx < len(sentence):
+        begin, end = get_following_operand(sentence, idx)
+        if sentence[begin] == "(" and sentence[end] == ")":
+
+            sentence[begin+1:end] = (simplified := simplify_parentheses(sentence[begin+1:end]))
+            end = begin + len(simplified) + 1
+
+            sub_operator = get_main_operator(sentence[begin+1:end])
+            if sub_operator == main_operator or sub_operator == "":
+                sentence = sentence[:begin] + sentence[begin+1:end] + sentence[end+1:]
+                idx = end - 1
+            else:
+                idx = end + 1
+        else:
+            idx = end + 1
+    return sentence
+
+def get_main_operator(sentence: list[str]) -> str | None:
+    """
+    Get the main operator that connects the operands in the sentence.
+
+    Return "" if the sentence is a single operand (no operator).
+
+    Return None if the sentence has more than one operator connecting the operands (more than one operator).
+
+    Return the operator if the sentence has only one operator connecting the operands (exactly one operator).
+
+    Args:
+        sentence (list[str]): The sentence to check.
+
+    Returns:
+        str | None: The main operator that connects the operands in the sentence.
+    """
+    operator = ""
+    idx = -1
+    while idx < len(sentence):
+        _, end = get_following_operand(sentence, idx)
+        idx = end + 1
+        if idx >= len(sentence):
+            break
+        if operator == "":
+            operator = sentence[idx]
+        elif sentence[idx] != operator:
+            return None
+    return operator
--- test/test_cnf_helper.py
+++ test/test_cnf_helper.py
@@ -1,17 +1,23 @@
 import unittest
-from cnf_helper import *
+from cnf_helper import add_parentheses_around_operator, and_or_tranformation, get_main_operator, get_previous_operand, get_following_operand, simplify_parentheses
 
-class TestCnfHelper(unittest.TestCase):
+class TestAndOrTransformation(unittest.TestCase):
+    """
+    Test and_or_transformation.
+
+    Args:
+        unittest (_type_): _description_
+    """
     def test_and_or_transformation(self):
         # Test biconditional elemination
         sentence = ['a', '<=>', 'b']
         sentence = and_or_tranformation(sentence)
-        self.assertEqual(sentence, [['(', '~', 'a', '||', 'b', ')'], '&', ['(', '~', 'b', '||', 'a', ')']])
+        self.assertEqual(sentence, ['(', '~', 'a', '||', 'b', ')', '&', '(', '~', 'b', '||', 'a', ')'])
 
         # Test implication elemination
         sentence = ['a', '=>', 'b']
         sentence = and_or_tranformation(sentence)
-        self.assertEqual(sentence, [['(', '~', 'a', '||', 'b', ')']])
+        self.assertEqual(sentence, ['(', '~', 'a', '||', 'b', ')'])
 
         # Test De Morgan's Law
         sentence = ['~', '(', 'a', '&', 'b', ')']
@@ -23,3 +29,117 @@ class TestCnfHelper(unittest.TestCase):
         sentence = and_or_tranformation(sentence)
         print(sentence)
         self.assertEqual(sentence, ['(', 'a', '||', 'c', ')', '&', '(', 'a', '||', 'd', ')', '&', '(', 'a', '||', 'e', ')'])
+
+class TestGetPreviousOperand(unittest.TestCase):
+    """
+    Test get_previous_operand.
+    """
+
+    def test_single(self):
+        sentence = ['a', '&', '(', 'b', '||', 'c', ')']
+        self.assertEqual(get_previous_operand(sentence, 1), (0, 0))
+
+    def test_parentheses(self):
+        sentence = ['(', 'b', '||', 'c', ')', '&', 'a']
+        self.assertEqual(get_previous_operand(sentence, 5), (0, 4))
+
+class TestGetFollowingOperand(unittest.TestCase):
+    """
+    Test get_following_operand.
+    """
+
+    def test_single(self):
+        sentence = ['a', '&', '(', 'b', '||', 'c', ')']
+        self.assertEqual(get_following_operand(sentence, 4), (5, 5))
+
+    def test_parentheses(self):
+        sentence = ['a', '&', '(', 'b', '||', 'c', ')']
+        self.assertEqual(get_following_operand(sentence, 1), (2, 6))
+
+    def test_negative_one(self):
+        sentence = ['a', '&', '(', 'b', '||', 'c', ')']
+        self.assertEqual(get_following_operand(sentence, -1), (0, 0))
+
+        sentence = ['(', 'b', '||', 'c', ')', '&', 'a']
+        self.assertEqual(get_following_operand(sentence, -1), (0, 4))
+
+class TestAddParenthesesAroundOperator(unittest.TestCase):
+    """
+    Test add_parentheses_around_operator.
+    """
+
+    def test_single_operator(self):
+        sentence = ['a', '&', 'b']
+        self.assertEqual(add_parentheses_around_operator(sentence, '&'), ['(', 'a', '&', 'b', ')'])
+
+        sentence = ['a', '&', 'b', '&', 'c']
+        self.assertEqual(add_parentheses_around_operator(sentence, '&'), ['(', 'a', '&', 'b', '&', 'c', ')'])
+
+    def test_multiple_operators(self):
+        sentence = ['a', '&', 'b', '&', 'c', '||', 'd', '||', 'e', '&', 'f']
+        self.assertEqual(add_parentheses_around_operator(sentence, '&'), ['(', 'a', '&', 'b', '&', 'c', ')', '||', 'd', '||', '(', 'e', '&', 'f', ')'])
+        self.assertEqual(add_parentheses_around_operator(sentence, '||'), ['a', '&', 'b', '&', '(', 'c', '||', 'd', '||', 'e', ')', '&', 'f'])
+
+    def test_already_have_parentheses(self):
+        sentence = ['(', 'a', '&', 'b', ')']
+        self.assertEqual(add_parentheses_around_operator(sentence, '&'), ['(', 'a', '&', 'b', ')'])
+
+class TestGetMainOperator(unittest.TestCase):
+    """
+    Test get_main_operator.
+    """
+
+    def test_single_operator(self):
+        sentence = ['a', '&', 'b']
+        self.assertEqual(get_main_operator(sentence), '&')
+
+        sentence = ['a', '||', 'b', '||', 'c']
+        self.assertEqual(get_main_operator(sentence), '||')
+
+    def test_multiple_operators(self):
+        sentence = ['a', '&', 'b', '||', 'c']
+        self.assertEqual(get_main_operator(sentence), None)
+
+    def test_no_operator(self):
+        sentence = ['a']
+        self.assertEqual(get_main_operator(sentence), "")
+
+    def test_single_operator_with_parentheses(self):
+        sentence = ['(', 'a', '&', 'b', ')', '||', 'c']
+        self.assertEqual(get_main_operator(sentence), '||')
+
+        sentence = ['(', 'a', '&', 'b', ')', '&', '(', 'c', '||', 'd', ')']
+        self.assertEqual(get_main_operator(sentence), '&')
+
+    def test_multiple_operators_with_parentheses(self):
+        sentence = ['(', 'a', '&', 'b', ')', '&', '(', 'c', '||', 'd', ')', '||', 'e']
+        self.assertEqual(get_main_operator(sentence), None)
+
+    def test_no_operator_with_parentheses(self):
+        sentence = ['(', 'a', '&', 'b', ')']
+        self.assertEqual(get_main_operator(sentence), '')
+
+class TestSimplifyParentheses(unittest.TestCase):
+    """
+    Test simplify_parentheses.
+    """
+
+    def test_simple(self):
+        sentence = ['(', 'a', '&', 'b', ')']
+        self.assertEqual(simplify_parentheses(sentence), ['a', '&', 'b'])
+
+    def test_multiple(self):
+        sentence = ['(', 'a', '&', '(', 'b', '&', 'c', ')', ')']
+        self.assertEqual(simplify_parentheses(sentence), ['a', '&', 'b', '&', 'c'])
+
+    def test_nested(self):
+        sentence = ['(', 'a', '&', '(', 'b', '&', '(', 'c', '&', 'd', ')', ')', ')']
+        self.assertEqual(simplify_parentheses(sentence), ['a', '&', 'b', '&', 'c', '&', 'd'])
+
+    def test_no_parentheses(self):
+        sentence = ['a', '&', 'b']
+        self.assertEqual(simplify_parentheses(sentence), ['a', '&', 'b'])
+
+    def test_different_operators(self):
+        sentence = ['(', 'a', '&', 'b', ')', '||', '(', 'c', '||', 'd', ')']
+        self.assertEqual(simplify_parentheses(sentence), ['(', 'a', '&', 'b', ')', '||', 'c', '||', 'd'])
