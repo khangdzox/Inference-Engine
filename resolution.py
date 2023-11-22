7b11f2eb5ae935e44bcefde38b117e6f50986b70
Hoai Nam <hoainamkin03@gmail.com>
2023-11-22 16:43:47 +0700

    update resolution

--- resolution.py
+++ resolution.py
@@ -1,14 +1,17 @@
 from cnf_helper import transform_to_cnf
 
-def resolution(knowledge_base, query):
-    clauses = transform_to_cnf(knowledge_base) + transform_to_cnf(['~' + query])
+def resolution(knowledge_base : list[list[str]], query : list[str]) -> bool:
+    cnf_kb = [transform_to_cnf(sentence) for sentence in knowledge_base]
+    cnf_query = transform_to_cnf(query)
+    clauses = cnf_kb + ['~' + cnf_query]
     new = []
     while True:
-        for [clause1, clause2] in clauses:
-            resolvents = resolve(clause1, clause2)
-            if [] in resolvents:
-                return True
-            new += resolvents
+        for clause1 in clauses:
+            for clause2 in clauses - clause1:
+                resolvents = resolve(clause1, clause2)
+                if [] in resolvents:
+                    return True
+                new += resolvents        
         if new in clauses:
             return False
         clauses += new
