cd782f931fd41e50a3115e22527b673d8935ee57
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2023-11-22 16:46:49 +0700

    Add resolve method

--- resolution.py
+++ resolution.py
@@ -12,3 +12,33 @@ def resolution(knowledge_base, query):
         if new in clauses:
             return False
         clauses += new
+
+def resolve(clause_1: list[str], clause_2: list[str]) -> list[str]:
+    """
+    Resolve two clauses.
+
+    Args:
+        clause_1 (list[str]): The first clause.
+        clause_2 (list[str]): The second clause.
+
+    Returns:
+        list[str]: The resolvents.
+    """
+    # negate all literal in clause 1
+    not_clause_1_set = {literal[1:] if literal[0] == '~' else '~' + literal for literal in clause_1}
+
+    # turn clause 2 into a set of literal
+    clause_2_set = set(clause_2)
+
+    # find the overlap between not_clause_1_set and clause_2_set
+    overlap = not_clause_1_set & clause_2_set
+
+    # remove the overlap from both sets
+    clause_2_set -= overlap
+    not_clause_1_set -= overlap
+
+    # negate all literal in not_clause_1_set
+    clause_1_set = {literal[1:] if literal[0] == '~' else '~' + literal for literal in not_clause_1_set}
+
+    # return the list of union of two clauses
+    return list(clause_1_set | clause_2_set)
