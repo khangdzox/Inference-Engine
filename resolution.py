b60900ae64516f5d885499d7b5de7b1ccec587fc
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2023-11-22 20:40:52 +0700

    Add comments and docstring

--- resolution.py
+++ resolution.py
@@ -1,24 +1,44 @@
 from cnf_helper import transform_to_cnf
 
-def resolution(knowledge_base : list[list[str]], query : list[str]) -> bool:
+def resolution_checking(knowledge_base : list[list[str]], query : list[str]) -> bool:
+    """
+    Check if the query is entailed by the knowledge base, using resolution algorithm.
+
+    This method check for entailment by contradiction:
+
+    If `KB |= query`, then `~KB || query` is true in all models, then `KB & ~query` will be unsatisfiable.
+
+    Args:
+        knowledge_base (list[list[str]]): The knowledge base.
+        query (list[str]): The query to be checked.
+
+    Returns:
+        bool: Whether the query is entailed by the knowledge base.
+    """
+    # transform knowledge base and query to CNF
     cnf_kb = [clauses for sentence in knowledge_base for clauses in transform_to_cnf(sentence)]
     cnf_not_query = transform_to_cnf(['~', '('] + query + [')'])
 
     # clause: KB & ~query
     clauses = cnf_kb + cnf_not_query
 
+    # empty list for resolved clauses
     new: list[list[str]] = []
 
     while True:
-        for i in range(len(clauses) - 1):
 
+        # for each pair of clauses, resolve them
+        for i in range(len(clauses) - 1):
             for j in range(i + 1, len(clauses)):
 
                 resolvent = resolve(clauses[i], clauses[j])
 
+                # if two clauses resolve to an empty clause, it means the sentence "KB & ~query" is unsatisfiable
+                # therefore, the query is entailed by the knowledge base. Return True
                 if not resolvent:
                     return True
 
+                # if resolvent is not in new, add it to new
                 if resolvent not in new:
                     new.append(resolvent)
 
@@ -33,30 +53,33 @@ def resolution(knowledge_base : list[list[str]], query : list[str]) -> bool:
 
 def resolve(clause_1: list[str], clause_2: list[str]) -> list[str]:
     """
-    Resolve two clauses.
+    Resolve two clauses using the resolution rule.
+
+    This method will remove all complementary literals between the two clause and only produce one new clause.
 
     Args:
         clause_1 (list[str]): The first clause.
         clause_2 (list[str]): The second clause.
 
     Returns:
-        list[str]: The resolvents.
+        list[str]: The resolvent.
     """
-    # negate all literal in clause 1
+    # negate all literals in clause 1
     not_clause_1_set = {literal[1:] if literal[0] == '~' else '~' + literal for literal in clause_1}
 
-    # turn clause 2 into a set of literal
+    # turn clause 2 into a set of literals
     clause_2_set = set(clause_2)
 
     # find the overlap between not_clause_1_set and clause_2_set
+    # which is the complementary literals between the two clauses
     overlap = not_clause_1_set & clause_2_set
 
     # remove the overlap from both sets
     clause_2_set -= overlap
     not_clause_1_set -= overlap
 
-    # negate all literal in not_clause_1_set
+    # negate all literals in not_clause_1_set to get the original literals
     clause_1_set = {literal[1:] if literal[0] == '~' else '~' + literal for literal in not_clause_1_set}
 
-    # return the list of union of two clauses
+    # return the union of two clause sets as a list
     return list(clause_1_set | clause_2_set)
--- test/test_resolution.py
+++ test/test_resolution.py
@@ -1,26 +1,29 @@
 import unittest
-from resolution import resolve, resolution
+from resolution import resolve, resolution_checking
 
 class TestResolution(unittest.TestCase):
+    """
+    Test resolution.
+    """
     def test_simple_true(self):
         kb = [['a', '=>', 'b'], ['b', '=>', 'c'], ['a']]
         query = ['c']
-        self.assertTrue(resolution(kb, query))
+        self.assertTrue(resolution_checking(kb, query))
 
     def test_simple_false(self):
         kb = [['a', '=>', 'b'], ['b', '&', 'd', '=>', 'c'], ['a'], [ '~', 'd']]
         query = ['c']
-        self.assertFalse(resolution(kb, query))
+        self.assertFalse(resolution_checking(kb, query))
 
     def test_complex_one(self):
         kb = [['p2', '=>', 'p3'], ['p3', '=>', 'p1'], ['c', '=>', 'e'], ['b', '&', 'e', '=>', 'f'], ['f', '&', 'g', '=>', 'h'], ['p1', '=>', 'd'], ['p1', '&', 'p3', '=>', 'c'], ['a'], ['b'], ['p2']]
         query = ['d']
-        self.assertTrue(resolution(kb, query))
+        self.assertTrue(resolution_checking(kb, query))
 
     def test_complex_two(self):
         kb = [['(', 'a', '<=>', '(', 'c', '=>', '~', 'd', ')', ')', '&', 'b', '&', '(', 'b', '=>', 'a', ')'], ['c'], ['~', 'f', '||', 'g']]
         query = ['~', 'd', '&', '(', '~', 'g', '=>', '~', 'f', ')']
-        self.assertTrue(resolution(kb, query))
+        self.assertTrue(resolution_checking(kb, query))
 
 class TestResolve(unittest.TestCase):
     """
