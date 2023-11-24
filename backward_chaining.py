12467121ac038ffbc344a2c1e5ec7342bad6b50b
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2023-11-24 17:54:32 +0700

    rename variables

--- backward_chaining.py
+++ backward_chaining.py
@@ -28,32 +28,32 @@ def backward_chaining_checking(knowledge_base: list[list[str]], query: str, exam
     # initialize entailed_symbols
     entailed_symbols = []
 
-    any_true = False
+    any_clause_true = False
     # this for loop with loop through the knowledge base to find a sentence that has the query as its conclusion
     # it will break if one of the sentences is proved to be true
-    for sentence in knowledge_base:
-        if conclusion(sentence) == query:
+    for clause in knowledge_base:
+        if conclusion(clause) == query:
 
-            all_true = True
+            all_symbol_true = True
             # this for loop will loop through the premise of the sentence to check if all of them are true
             # it will break if one of the premise is proved to be false
-            for symbol in premise(sentence):
+            for symbol in premise(clause):
                 truth, entailed = backward_chaining_checking(knowledge_base, symbol, examined_symbols + [query])
 
                 # if one of the premise is false, then the sentence is false
                 if truth:
                     entailed_symbols += entailed
                 else:
-                    all_true = False
+                    all_symbol_true = False
                     break
 
             # if all of the premise are true, then the sentence is true
-            if all_true:
-                any_true = True
+            if all_symbol_true:
+                any_clause_true = True
                 break
 
     # if one of the sentences is true, then the query is entailed by the knowledge base
-    if any_true:
+    if any_clause_true:
         return True, set(entailed_symbols + [query])
 
     # if none of the sentences is true, then the query is not entailed by the knowledge base
