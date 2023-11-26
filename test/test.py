e9bf9b233d0e10548d0ab41d5d4c97e26fb5a7af
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2023-11-26 14:29:25 +0700

    Add docstring and comment. add test data

--- test/test.py
+++ test/test.py
@@ -6,8 +6,15 @@ from resolution import resolution_checking
 from dpll import dpll_checking
 
 class TestTruthTableChecking(unittest.TestCase):
+    """
+    Test truth_table_checking.
+    """
     def test_case_1(self):
         # Horn-form knowledge base
+        # TELL
+        # p2 => p3; p3 => p1; c => e; b&e => f; f&g => h; p1 => d; p1&p3 => c; a; b; p2;
+        # ASK
+        # d
         kb = [['p2', '=>', 'p3'], ['p3', '=>', 'p1'], ['c', '=>', 'e'], ['b', '&', 'e', '=>', 'f'], ['f', '&', 'g', '=>', 'h'], ['p1', '=>', 'd'], ['p1', '&', 'p3', '=>', 'c'], ['a'], ['b'], ['p2']]
         query = ['d']
         symbols = ['p2', 'p3', 'p1', 'c', 'e', 'b', 'f', 'g', 'h', 'd', 'a']
@@ -17,14 +24,22 @@ class TestTruthTableChecking(unittest.TestCase):
 
     def test_case_2(self):
         # General knowledge base
+        # TELL
+        # (a <=> (c => ~d)) & b & (b => a); c; ~f || g;
+        # ASK
+        # d
         kb = [['(', 'a', '<=>', '(', 'c', '=>', '~', 'd', ')', ')', '&', 'b', '&', '(', 'b', '=>', 'a', ')'], ['c'], ['~', 'f', '||', 'g']]
         query = ['d']
         symbols = ['a', 'c', 'd', 'b', 'f', 'g']
-        result, count = truth_table_checking(kb, query, symbols)
+        result, _ = truth_table_checking(kb, query, symbols)
         self.assertFalse(result)
 
     def test_case_3(self):
         # General knowledge base
+        # TELL
+        # (a <=> (c => ~d)) & b & (b => a); c; ~f || g;
+        # ASK
+        # ~d & (~g => ~f)
         kb = [['(', 'a', '<=>', '(', 'c', '=>', '~', 'd', ')', ')', '&', 'b', '&', '(', 'b', '=>', 'a', ')'], ['c'], ['~', 'f', '||', 'g']]
         query = ['~', 'd', '&', '(', '~', 'g', '=>', '~', 'f', ')']
         symbols = ['a', 'c', 'd', 'b', 'f', 'g']
@@ -37,7 +52,7 @@ class TestTruthTableChecking(unittest.TestCase):
         kb = [['x', '=>', 'y'], ['y', '=>', 'z'], ['z', '=>', 'w'], ['a', '&', 'b', '=>', 'x'], ['c', '&', 'd', '=>', 'a'], ['w', '&', 'e', '=>', 'c'], ['~', 'b']]
         query = ['e']
         symbols = ['x', 'y', 'z', 'w', 'a', 'b', 'c', 'd', 'e']
-        result, count = truth_table_checking(kb, query, symbols)
+        result, _ = truth_table_checking(kb, query, symbols)
         self.assertFalse(result)
 
     def test_case_5(self):
@@ -45,42 +60,51 @@ class TestTruthTableChecking(unittest.TestCase):
         kb = [['p', '=>', 'q'], ['q', '&', 'r'], ['s', '||', '~', 't'], ['u', '&', 'v', '<=>', 'p'], ['r', '&', 'w', '=>', 's'], ['~', 'u']]
         query = ['t', '||', '~', 'v']
         symbols = ['p', 'q', 'r', 's', 't', 'u', 'v', 'w']
-        result, count = truth_table_checking(kb, query, symbols)
+        result, _ = truth_table_checking(kb, query, symbols)
         self.assertFalse(result)
 
 class TestForwardChaining(unittest.TestCase):
+    """
+    Test forward_chaining_checking.
+    """
     def test_case_1(self):
         # Horn-form knowledge base
         kb = [['p2', '=>', 'p3'], ['p3', '=>', 'p1'], ['c', '=>', 'e'], ['b', '&', 'e', '=>', 'f'], ['f', '&', 'g', '=>', 'h'], ['p1', '=>', 'd'], ['p1', '&', 'p3', '=>', 'c'], ['a'], ['b'], ['p2']]
         query = 'd'
-        result, set = forward_chaining_checking(kb, query)
+        result, inferred = forward_chaining_checking(kb, query)
         self.assertTrue(result)
-        self.assertEqual(set, {'a', 'p3', 'p1', 'd', 'p2', 'b'})
+        self.assertEqual(inferred, {'a', 'p3', 'p1', 'd', 'p2', 'b'})
 
     def test_case_2(self):
         # Horn-form knowledge base
         kb = [['x', '=>', 'y'], ['y', '=>', 'z'], ['z', '=>', 'w'], ['a', '&', 'b', '=>', 'x'], ['c', '&', 'd', '=>', 'a'], ['w', '&', 'e', '=>', 'c'], ['~', 'b']]
         query = 'e'
-        result, set = forward_chaining_checking(kb, query)
+        result, _ = forward_chaining_checking(kb, query)
         self.assertFalse(result)
 
 class TestBackwardChaining(unittest.TestCase):
+    """
+    Test backward_chaining_checking.
+    """
     def test_case_1(self):
         # Horn-form knowledge base
         kb = [['p2', '=>', 'p3'], ['p3', '=>', 'p1'], ['c', '=>', 'e'], ['b', '&', 'e', '=>', 'f'], ['f', '&', 'g', '=>', 'h'], ['p1', '=>', 'd'], ['p1', '&', 'p3', '=>', 'c'], ['a'], ['b'], ['p2']]
         query = 'd'
-        result, set = backward_chaining_checking(kb, query)
+        result, inferred = backward_chaining_checking(kb, query)
         self.assertTrue(result)
-        self.assertEqual(set, {'p2', 'p3', 'p1', 'd'})
+        self.assertEqual(inferred, {'p2', 'p3', 'p1', 'd'})
 
     def test_case_2(self):
         # Horn-form knowledge base
         kb = [['x', '=>', 'y'], ['y', '=>', 'z'], ['z', '=>', 'w'], ['a', '&', 'b', '=>', 'x'], ['c', '&', 'd', '=>', 'a'], ['w', '&', 'e', '=>', 'c'], ['~', 'b']]
         query = 'e'
-        result, set = backward_chaining_checking(kb, query)
+        result, _ = backward_chaining_checking(kb, query)
         self.assertFalse(result)
 
 class TestResolutionChecking(unittest.TestCase):
+    """
+    Test resolution_checking.
+    """
     def test_case_1(self):
         # Horn-form knowledge base
         kb = [['p2', '=>', 'p3'], ['p3', '=>', 'p1'], ['c', '=>', 'e'], ['b', '&', 'e', '=>', 'f'], ['f', '&', 'g', '=>', 'h'], ['p1', '=>', 'd'], ['p1', '&', 'p3', '=>', 'c'], ['a'], ['b'], ['p2']]
@@ -117,6 +141,9 @@ class TestResolutionChecking(unittest.TestCase):
         self.assertFalse(result)
 
 class TestDPLLChecking(unittest.TestCase):
+    """
+    Test dpll_checking.
+    """
     def test_case_1(self):
         # Horn-form knowledge base
         kb = [['p2', '=>', 'p3'], ['p3', '=>', 'p1'], ['c', '=>', 'e'], ['b', '&', 'e', '=>', 'f'], ['f', '&', 'g', '=>', 'h'], ['p1', '=>', 'd'], ['p1', '&', 'p3', '=>', 'c'], ['a'], ['b'], ['p2']]
@@ -156,7 +183,6 @@ class TestDPLLChecking(unittest.TestCase):
         symbols = ['p', 'q', 'r', 's', 't', 'u', 'v', 'w']
         result = dpll_checking(kb, query, symbols)
         self.assertFalse(result)
-
 
 if __name__ == '__main__':
     unittest.main()
